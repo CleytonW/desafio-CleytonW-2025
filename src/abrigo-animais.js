260938a7eecf1111a0ae7e732a8d8cbc2b398816
Cleyton Melo <cleytonw11@gmail.com>
2025-09-07 22:16:03 -0300

    Adicionando dados dos animais e seus brinquedos

---
+++ src/abrigo-animais.js
@@ -0,0 +1,19 @@
+class AbrigoAnimais {
+  constructor() {
+    this.animais = [
+      { nome: "Rex", tipo: "cão", brinquedos: ["BOLA", "RATO"] },
+      { nome: "Mimi", tipo: "gato", brinquedos: ["BOLA", "LASER"] },
+      { nome: "Fofo", tipo: "gato", brinquedos: ["BOLA", "RATO", "LASER"] },
+      { nome: "Zero", tipo: "gato", brinquedos: ["RATO", "BOLA"] },
+      { nome: "Bola", tipo: "cão", brinquedos: ["CAIXA", "NOVELO"] },
+      { nome: "Bebe", tipo: "cão", brinquedos: ["LASER", "RATO", "BOLA"] },
+      { nome: "Loco", tipo: "jabuti", brinquedos: ["SKATE", "RATO"] },
+    ];
+  }
+
+  encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
+    return [];
+  }
+}
+
+export { AbrigoAnimais as AbrigoAnimais };
