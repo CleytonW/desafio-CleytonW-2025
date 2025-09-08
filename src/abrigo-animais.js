dea3647feb5e0d63e36a2a910e62b2af6fefc0c0
Cleyton Melo <cleytonw11@gmail.com>
2025-09-08 14:24:48 -0300

    Verificando se a pessoa possui brinquedos que os animais gostam

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -12,9 +12,26 @@ class AbrigoAnimais {
   }
 
   encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
-    const lista1 = brinquedosPessoa1.split(',');
-    const lista2 = brinquedosPessoa2.split(',');
-    const ordem = ordemAnimais.split(',');
+    const lista1 = brinquedosPessoa1.split(",");
+    const lista2 = brinquedosPessoa2.split(",");
+    const ordem = ordemAnimais.split(",");
+
+    const pessoa1 = [];
+    const pessoa2 = [];
+
+    for (const animal of this.animais) {
+      for (const brinquedo of animal.brinquedos) {
+        if (lista1.includes(brinquedo) && !pessoa1.includes(animal.nome)) {
+          pessoa1.push(animal.nome);
+        }
+        if (lista2.includes(brinquedo) && !pessoa2.includes(animal.nome)) {
+          pessoa2.push(animal.nome);
+        }
+      }
+    }
+    return { 
+      pessoa1, pessoa2 
+    };
   }
 }
 
